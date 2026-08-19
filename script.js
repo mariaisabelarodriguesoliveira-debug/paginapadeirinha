df264176ec655d1c2e180cbd3f4c9ed42ff802e6
mariaisabelarodriguesoliveira-debug <mariaisabelarodriguesoliveira@gmail.com>
2026-08-19 20:53:23 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// Seleciona todos os botões das categorias
+const botoesCategoria = document.querySelectorAll(".botao-categoria");
 
+
+// Adiciona uma função para cada botão
+botoesCategoria.forEach(function(botao) {
+
+    botao.addEventListener("click", function() {
+
+        // Pega a categoria onde o botão está
+        const categoria = botao.parentElement;
+
+
+        // Abre ou fecha a categoria
+        categoria.classList.toggle("aberta");
+
+    });
+
+});
